f45684848be79e0685ddf27ecc90725f1d003506
rovinkyk <kykrovin@gmail.com>
2023-08-17 13:51:56 +0530

    Initial commit

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req,res)=> {
     res.send("Hello Node Api")
 })
 
+app.get('/blog', (req,res)=> {
+    res.send("Hello Blog by Rovin")
+})
+
 app.listen(3000, ()=>{
     console.log('listening on port 3000')
 })
